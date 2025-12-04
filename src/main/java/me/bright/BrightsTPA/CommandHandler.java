bed72f6cf0a9ae1052a259af3d82a13a9c8796bd
BreightForever <vanhtrau09@gmail.com>
2025-12-04 04:57:32 +0000

    Initial Commit

--- src/main/java/me/bright/BrightsTPA/CommandHandler.java
+++ src/main/java/me/bright/BrightsTPA/CommandHandler.java
@@ -11,7 +11,7 @@ import static me.bright.BrightsTPA.Format.String.send;
 public record CommandHandler(HandleExecutor plugin) implements CommandExecutor {
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String @NotNull [] args) {
+    public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String [] args) {
 
         switch (command.getName().toLowerCase()) {
 
@@ -23,7 +23,6 @@ public record CommandHandler(HandleExecutor plugin) implements CommandExecutor {
                 plugin.handleTpaCommand((Player) sender, args);
                 return true;
             }
-
             case "tpahere" -> {
                 if (!(sender instanceof Player)) {
                     send(sender, "&cOnly players may use this command!");
@@ -32,7 +31,6 @@ public record CommandHandler(HandleExecutor plugin) implements CommandExecutor {
                 plugin.handleTpahereCommand((Player) sender, args);
                 return true;
             }
-
             case "tpaccept", "tpyes" -> {
                 if (!(sender instanceof Player)) {
                     send(sender, "&cOnly players may use this command!");
@@ -41,7 +39,6 @@ public record CommandHandler(HandleExecutor plugin) implements CommandExecutor {
                 plugin.handleTpaAcceptCommand((Player) sender, args);
                 return true;
             }
-
             case "tpdeny", "tpno" -> {
                 if (!(sender instanceof Player)) {
                     send(sender, "&cOnly players may use this command!");
@@ -50,12 +47,15 @@ public record CommandHandler(HandleExecutor plugin) implements CommandExecutor {
                 plugin.handleTpaDenyCommand((Player) sender, args);
                 return true;
             }
-
             case "brightstpa" -> {
-                plugin.handleReloadCommand((Player) sender);
+                if (args.length > 0 && args[0].equalsIgnoreCase("version")) {
+                    plugin.handleVersionCommand((Player) sender);
+                }
+                else if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+                    plugin.handleReloadCommand((Player) sender);
+                }
                 return true;
             }
-
             case "tpacancel" -> {
                 if (!(sender instanceof Player)) {
                     send(sender, "&cOnly players may use this command!");
@@ -64,7 +64,6 @@ public record CommandHandler(HandleExecutor plugin) implements CommandExecutor {
                 plugin.handleTpaCancelCommand((Player) sender, args);
                 return true;
             }
-
         }
         return false;
     }

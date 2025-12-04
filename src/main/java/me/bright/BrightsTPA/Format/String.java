3d538427b000d7900fbfc85dd115e9d608293f88
BreightForever <vanhtrau09@gmail.com>
2025-12-04 15:11:41 +0000

    Initial Commit

--- src/main/java/me/bright/BrightsTPA/Format/String.java
+++ src/main/java/me/bright/BrightsTPA/Format/String.java
@@ -1,11 +1,20 @@
 package me.bright.BrightsTPA.Format;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import static me.bright.BrightsTPA.BrightsTPA.LEGACY;
 
 public class String {
     public static void send(CommandSender sender, java.lang.String msg, Object... args) {
-        sender.sendMessage(LEGACY.deserialize(java.lang.String.format(msg, args)));
+
+        msg = java.lang.String.format(msg, args);
+
+        if (sender instanceof Player player) {
+            msg = PlaceholderAPI.setPlaceholders(player, msg);
+        }
+
+        sender.sendMessage(LEGACY.deserialize(msg));
     }
 }
